7b913bcea57e776aa90fa4597f22c27dee76d473
ISTIAK AL IMRAN <22301040@bracu.ac.bd>
2023-10-15 19:58:29 +0600

    firebase added

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// eslint-disable-next-line no-unused-vars
 const firebaseConfig = {
   apiKey: "AIzaSyAvkuaCfXVqkiyFgAYxmrlQkjvS5SsmL80",
   authDomain: "loginproject-ddd1c.firebaseapp.com",
@@ -14,4 +14,3 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
