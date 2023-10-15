d36650a9356e5cee70c3d2700d80b4ad8fa734f8
IstiakImran <toufique.hassan.71@gmail.com>
2023-10-16 01:31:49 +0600

    Learning done for this phase

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,26 +1,45 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import React, { useState } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import app from '../../Firebase/firebase.init';
 
 const Login = () => {
+    const [user, setUser] = useState(null)
     const auth = getAuth(app)
     const provider = new GoogleAuthProvider()
     console.log(app)
     const GoogleSignIn = () => {
         signInWithPopup(auth, provider)
             .then(result => {
-                const user = result.user;
-                console.log(user);
+                const LoggedUser = result.user;
+                setUser(LoggedUser);
+                console.log(LoggedUser);
             })
             .catch(error => {
                 console.log('error:', error.message)
             })
-
+    };
+    const GoogleSignOut = () => {
+        signOut(auth, provider)
+            .then(resut => {
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error)
+            })
     }
     return (
         <div>
-            <button onClick={GoogleSignIn}>Sign In</button>
+            {user ?
+                <button onClick={GoogleSignOut}>Sign Out</button> :
+                <button onClick={GoogleSignIn}>Sign In</button>}
+            {user &&
+                <div>
+                    <h1>
+                        {user.displayName}
+                    </h1>
+                </div>}
+
         </div>
     );
 };
